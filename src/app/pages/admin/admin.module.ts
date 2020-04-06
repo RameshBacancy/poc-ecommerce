876221a9afdfcb2cb6f1ef1117c77205c09a795b
kishanlakhani <kishan.lakhani@bacancytechnology.com>
2020-04-06 20:49:07 +0530

    add dashboard component and add some features

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -14,6 +14,7 @@ import { AdminComponent } from './admin.component';
 import { GuideTestComponent } from './guide-test/guide-test.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardTestComponent } from './dashboard-test/dashboard-test.component';
 @NgModule({
   declarations: [
     AdminComponent,
@@ -23,7 +24,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     AddEditCategoryComponent,
     GuideTestComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    DashboardTestComponent,
+    DashboardTestComponent
   ],
   imports: [
     CommonModule,
